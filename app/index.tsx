37651ab4214aeaa5787b51cfe4ec1d0c0364c112
Kateryna Troyan <ket.tro@gmail.com>
2026-02-05 18:20:39 +0200

    day 1: react native basics and jsx practice

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,23 @@
-import { View, Text,  StyleSheet, Pressable } from "react-native";
-import { useState } from "react";
+import { Text, View } from "react-native";
 
 export default function Index() {
+  const name = "Anna";
+  const profession = "Developer";
+  const getGreeting = () => {
+    return "Welcome tio React";
+  };
+  let count = 0;
+  const increase = () => {
+    count += 1;
+    console.log(count);
+  };
+  increase();
+  return (
+    <View>
+      <Text>Hello, {name}!</Text>
+      <Text>Profession: {profession}</Text>
+      <Text>{getGreeting()}</Text>
+      console.log(count);
+    </View>
+  );
 }
